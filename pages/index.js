2e6270729ddca82e87bd9d71def393f293cad15f
Ricardo <ricardoaqf.dev@gmail.com>
2021-11-09 16:52:02 -0400

    made the accordion reusable

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,33 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AddIcon from "@mui/icons-material/Add";
+import { styled } from "@mui/material/styles";
+
+const AccordionStyled = styled((props) => <Accordion {...props} />)(
+  ({ theme }) => ({
+    backgroundColor: "#343434",
+    color: "white",
+    margin: "5px 0",
+    "& .MuiAccordionSummary-root": {
+      borderBottom: "1px solid black",
+    },
+    // "& .MuiAccordionSummary-content.Mui-expanded, .MuiAccordionSummary-content":
+    //   {
+    //     margin: 0,
+    //   },
+  })
+);
+
+const AccordionSummaryStyled = styled((props) => (
+  <AccordionSummary
+    expandIcon={<AddIcon sx={{ color: "white", fontSize: "3rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiAccordionSummary-content.Mui-expanded, .MuiAccordionSummary-content": {
+    margin: 0,
+  },
+}));
 
 const Home = () => {
   return (
@@ -560,22 +587,13 @@ const Home = () => {
               >
                 Frequently Asked Questions
               </h2>
-              <Accordion
-                sx={{
-                  backgroundColor: "#343434",
-                  color: "white",
-                  "& .MuiAccordionSummary-content.Mui-expanded": { margin: 0 },
-                }}
-              >
-                <AccordionSummary
-                  expandIcon={
-                    <AddIcon sx={{ color: "white", fontSize: "3rem" }} />
-                  }
+              <AccordionStyled disableGutters square>
+                <AccordionSummaryStyled
                   aria-controls="panel1a-content"
                   id="panel1a-header"
                 >
                   <h3>What is Netflix?</h3>
-                </AccordionSummary>
+                </AccordionSummaryStyled>
                 <AccordionDetails>
                   <p>
                     Netflix is a streaming service that offers a wide variety of
@@ -589,39 +607,98 @@ const Home = () => {
                     are added every week!
                   </p>
                 </AccordionDetails>
-              </Accordion>
-              <Accordion
-                sx={{
-                  backgroundColor: "#343434",
-                  color: "white",
-                  "& .MuiPaper-root-MuiAccordion-root.Mui-expanded": {
-                    margin: 0,
-                  },
-                }}
-              >
-                <AccordionSummary
-                  expandIcon={
-                    <AddIcon sx={{ color: "white", fontSize: "3rem" }} />
-                  }
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
+              </AccordionStyled>
+              <AccordionStyled disableGutters square>
+                <AccordionSummaryStyled
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
                 >
-                  <h3>What is Netflix?</h3>
-                </AccordionSummary>
+                  <h3>How much does Netflix cost?</h3>
+                </AccordionSummaryStyled>
                 <AccordionDetails>
                   <p>
-                    Netflix is a streaming service that offers a wide variety of
-                    award-winning TV shows, movies, anime, documentaries, and
-                    more on thousands of internet-connected devices.
-                  </p>
-                  <p>
-                    You can watch as much as you want, whenever you want without
-                    a single commercial – all for one low monthly price. There's
-                    always something new to discover and new TV shows and movies
-                    are added every week!
+                    Watch Netflix on your smartphone, tablet, Smart TV, laptop,
+                    or streaming device, all for one fixed monthly fee. Plans
+                    range from USD7.99 to USD13.99 a month. No extra costs, no
+                    contracts.
                   </p>
                 </AccordionDetails>
-              </Accordion>
+              </AccordionStyled>
+              <AccordionStyled disableGutters square>
+                <AccordionSummaryStyled
+                  aria-controls="panel3a-content"
+                  id="panel3a-header"
+                >
+                  <h3>Where can I watch?</h3>
+                </AccordionSummaryStyled>
+                <AccordionDetails>
+                  <p>
+                    Watch anywhere, anytime. Sign in with your Netflix account
+                    to watch instantly on the web at netflix.com from your
+                    personal computer or on any internet-connected device that
+                    offers the Netflix app, including smart TVs, smartphones,
+                    tablets, streaming media players and game consoles.
+                  </p>
+                  <p>
+                    You can also download your favorite shows with the iOS,
+                    Android, or Windows 10 app. Use downloads to watch while
+                    you're on the go and without an internet connection. Take
+                    Netflix with you anywhere.
+                  </p>
+                </AccordionDetails>
+              </AccordionStyled>
+              <AccordionStyled disableGutters square>
+                <AccordionSummaryStyled
+                  aria-controls="panel4a-content"
+                  id="panel4a-header"
+                >
+                  <h3>How do I cancel?</h3>
+                </AccordionSummaryStyled>
+                <AccordionDetails>
+                  <p>
+                    Netflix is flexible. There are no pesky contracts and no
+                    commitments. You can easily cancel your account online in
+                    two clicks. There are no cancellation fees – start or stop
+                    your account anytime.
+                  </p>
+                </AccordionDetails>
+              </AccordionStyled>
+              <AccordionStyled disableGutters square>
+                <AccordionSummaryStyled
+                  aria-controls="panel5a-content"
+                  id="panel5a-header"
+                >
+                  <h3>What can I watch on Netflix?</h3>
+                </AccordionSummaryStyled>
+                <AccordionDetails>
+                  <p>
+                    Netflix has an extensive library of feature films,
+                    documentaries, TV shows, anime, award-winning Netflix
+                    originals, and more. Watch as much as you want, anytime you
+                    want.
+                  </p>
+                </AccordionDetails>
+              </AccordionStyled>
+              <AccordionStyled disableGutters square>
+                <AccordionSummaryStyled
+                  aria-controls="panel6a-content"
+                  id="panel6a-header"
+                >
+                  <h3>Is Netflix good for kids?</h3>
+                </AccordionSummaryStyled>
+                <AccordionDetails>
+                  <p>
+                    The Netflix Kids experience is included in your membership
+                    to give parents control while kids enjoy family-friendly TV
+                    shows and movies in their own space.
+                  </p>
+                  <p>
+                    Kids profiles come with PIN-protected parental controls that
+                    let you restrict the maturity rating of content kids can
+                    watch and block specific titles you don’t want kids to see.
+                  </p>
+                </AccordionDetails>
+              </AccordionStyled>
             </Box>
           </div>
         </div>
