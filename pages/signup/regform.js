a3af96d89773288e8b10ebd9c5337afe21164250
Ricardo <ricardoaqf.dev@gmail.com>
2021-11-16 16:57:51 -0400

    regform page

---
+++ pages/signup/regform.js
@@ -0,0 +1,30 @@
+import Box from "@mui/material/Box";
+import { useFormik } from "formik";
+
+const Regform = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <Box>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+      </form>
+    </Box>
+  );
+};
+
+export default Regform;
