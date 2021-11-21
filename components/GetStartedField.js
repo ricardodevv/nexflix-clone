0a7668b082d85524f98610d6e4dad4f1bac3c74a
Ricardo Quintero <ricardoaqf.dev@gmail.com>
2021-11-21 00:00:04 -0400

    before making the EmailInput styled component

--- components/GetStartedField.js
+++ components/GetStartedField.js
@@ -3,6 +3,7 @@
  */
 import { useState, useEffect, useRef } from "react";
 import { css } from "@emotion/react";
+import { useFormik } from "formik";
 import styled from "@emotion/styled";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Button from "@mui/material/Button";
@@ -16,18 +17,20 @@ const TextFieldStyled = styled((props) => <form {...props} />)((theme) => ({
   width: "100%",
 }));
 
-const GetStartedButton = styled(Button)(({ theme }) => ({
-  fontSize: "1.5rem",
-  padding: "0 1rem",
-  textTransform: "none",
-  backgroundColor: "#e50914",
-  borderRadius: 0,
-  color: "white",
-  "&:hover": {
-    transition: "none",
+const GetStartedButton = styled((props) => <Button {...props}></Button>)(
+  ({ theme }) => ({
+    fontSize: "1.5rem",
+    padding: "0 1rem",
+    textTransform: "none",
     backgroundColor: "#e50914",
-  },
-}));
+    borderRadius: 0,
+    color: "white",
+    "&:hover": {
+      transition: "none",
+      backgroundColor: "#e50914",
+    },
+  })
+);
 
 const Label = styled.label`
   position: absolute;
@@ -41,22 +44,50 @@ const Label = styled.label`
       : null};
 `;
 
+const EmailInput = styled.input`
+  ${(props) => {}}
+`;
+
 const GetStartedField = () => {
   const [email, setEmail] = useState("");
   const [focusEmail, setFocusEmail] = useState(false);
   const wrapperRef = useRef(null);
 
-  console.log(focusEmail);
+  const validate = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = "Email required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, false);
     return () => {
       document.removeEventListener("click", handleClickOutside, false);
     };
-  }, []);
+  });
 
   const handleClickOutside = (e) => {
-    if (e.target.id !== "inpuuut" && focusEmail === true) {
+    if (
+      e.target.id !== "inpuuut" &&
+      focusEmail === true &&
+      email.length === 0
+    ) {
       setFocusEmail(!focusEmail);
     }
   };
@@ -72,7 +103,7 @@ const GetStartedField = () => {
   };
 
   return (
-    <TextFieldStyled>
+    <TextFieldStyled onSubmit={formik.handleSubmit}>
       <div
         css={css`
           flex: 1;
@@ -89,16 +120,18 @@ const GetStartedField = () => {
       >
         <Label htmlFor="email" focusEmail={focusEmail}></Label>
         <input
-          onChange={(e) => handleEmailChange(e)}
-          value={email}
-          id="inpuuut"
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
           css={css`
-            font-size: 1.25rem;
+            font-size: 1rem;
             outline: none;
             border: none;
             width: 100%;
             padding: 1.25rem 0.5rem;
-            margin: 0;
+            margin-top: 5px;
             height: fit-content;
             z-index: 1;
             background-color: transparent;
@@ -106,20 +139,19 @@ const GetStartedField = () => {
           onClick={(e) => focusEmailInput(e)}
           ref={wrapperRef}
         />
+        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
       </div>
-      <Link href={{ pathname: "/signup/registration", query: "" }}>
-        <GetStartedButton>
-          <p
-            css={css`
-              margin: 0;
-              padding-right: 5px;
-            `}
-          >
-            Get Started
-          </p>
-          <ArrowForwardIosIcon />
-        </GetStartedButton>
-      </Link>
+      <GetStartedButton type="submit">
+        <p
+          css={css`
+            margin: 0;
+            padding-right: 5px;
+          `}
+        >
+          Get Started
+        </p>
+        <ArrowForwardIosIcon />
+      </GetStartedButton>
     </TextFieldStyled>
   );
 };
