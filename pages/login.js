96a22d288fd16aa9c6269b502097b00a48186988
Ricardo Quintero <ricardoaqf.dev@gmail.com>
2021-12-21 16:57:36 -0400

    sign in provider page

--- pages/login.js
+++ pages/login.js
@@ -14,71 +14,71 @@ import { useSession, getSession, signIn } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
+const containerStyled = css`
+  flex: 1;
+  position: relative;
+  display: flex;
+  margin: 0.3rem 0;
+  align-items: center;
+  border: 1px solid gray;
+  border-radius: 2px;
+`;
+
+const labelStyled = css`
+  position: absolute;
+  top: -0.2rem;
+  width: 100%;
+  transition: 0.2s;
+  color: white;
+  z-index: 2;
+`;
+
+const labelTransition = css`
+  top: -1.1rem;
+  transition: 0.1s;
+  font-size: 0.8rem;
+  font-weight: 700;
+`;
+
+const inputStyled = css`
+  font-size: 1rem;
+  outline: none;
+  border: none;
+  width: 100%;
+  padding: 1.3rem 0.5rem;
+  height: fit-content;
+  z-index: 1;
+  background-color: #333;
+  border-radius: 3px;
+`;
+
+const inputError = css`
+  border: 1px solid red;
+`;
+
+const buttonStyled = {
+  fontSize: "1.5rem",
+  textTransform: "none",
+  backgroundColor: "#e50914",
+  borderRadius: 0,
+  color: "white",
+  marginTop: "2rem",
+  "&:hover": {
+    transition: "none",
+    backgroundColor: "#e50914",
+  },
+};
+
 const login = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
 
   const loginUser = async (email, password) => {
     try {
-      signIn();
+      signIn("google");
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const containerStyled = css`
-    flex: 1;
-    position: relative;
-    display: flex;
-    margin: 0.3rem 0;
-    align-items: center;
-    border: 1px solid gray;
-    border-radius: 2px;
-  `;
-
-  const labelStyled = css`
-    position: absolute;
-    top: -0.2rem;
-    width: 100%;
-    transition: 0.2s;
-    color: white;
-    z-index: 2;
-  `;
-
-  const labelTransition = css`
-    top: -1.1rem;
-    transition: 0.1s;
-    font-size: 0.8rem;
-    font-weight: 700;
-  `;
-
-  const inputStyled = css`
-    font-size: 1rem;
-    outline: none;
-    border: none;
-    width: 100%;
-    padding: 1.3rem 0.5rem;
-    height: fit-content;
-    z-index: 1;
-    background-color: #333;
-    border-radius: 3px;
-  `;
-
-  const inputError = css`
-    border: 1px solid red;
-  `;
-
-  const buttonStyled = {
-    fontSize: "1.5rem",
-    textTransform: "none",
-    backgroundColor: "#e50914",
-    borderRadius: 0,
-    color: "white",
-    marginTop: "2rem",
-    "&:hover": {
-      transition: "none",
-      backgroundColor: "#e50914",
-    },
   };
 
   if (session) {
